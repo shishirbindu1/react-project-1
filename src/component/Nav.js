b05dcfded4ea550ad3c14b13910dfb6009329a6c
shishir <bindushishir004@gmail.com>
2023-12-31 13:36:06 +0545

    This is the beginning

---
+++ src/component/Nav.js
@@ -0,0 +1,13 @@
+// Nav.js
+
+import React from 'react';
+import logo from '../Images/brand_logo.png'
+const Nav = () => {
+  return (
+    <div>
+      <img src={logo} alt ="logo" />
+    </div>
+  );
+};
+
+export default Nav;
